dd40e8622a03ca021423dde9f6a3623506f57552
Bence A. Tóth <tothab@gmail.com>
2022-06-16 11:33:45 +0200

    Added implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const fakeProducts = [
+  {
+    id: 1,
+    price: 100,
+    name: "Product 1",
+  },
+  {
+    id: 2,
+    price: 200,
+    name: "Product 2",
+  },
+  {
+    id: 3,
+    price: 250,
+    name: "Product 3",
+  },
+];
+
+const ProductLine = ({ product, onAdd, onRemove }) => {
+  return (
+    <>
+      <h1>{product.name}</h1>
+      <p>Price: {product.price} DKK</p>
+      <button onClick={onAdd}>+</button>
+      <span>{product.count}</span>
+      <button onClick={onRemove}>-</button>
+      <p>Line price: {product.price * product.count} DKK</p>
+    </>
+  );
+};
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(
+      fakeProducts.map((products) => ({
+        ...products,
+        count: 0,
+      }))
+    );
+  }, []);
+
+  const addProductToCart = (productId) => {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count + 1,
+          };
+        } else {
+          return product;
+        }
+      })
+    );
+  };
+
+  const removeProductFromCart = (productId) => {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count - 1,
+          };
+        } else {
+          return product;
+        }
+      })
+    );
+  };
+
+  const calculateTotal = () => {
+    return products
+      .map((products) => products.price * products.count)
+      .reduce((a, b) => a + b, 0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((product) => (
+        <div key={product.id}>
+          <ProductLine
+            product={product}
+            onAdd={() => {
+              addProductToCart(product.id);
+            }}
+            onRemove={() => {
+              removeProductFromCart(product.id);
+            }}
+          />
+        </div>
+      ))}
+      <p>Total: {calculateTotal()} DKK</p>
+      <p>Total including shipping: {calculateTotal() + 100} DKK</p>
     </div>
   );
-}
+};
 
 export default App;
